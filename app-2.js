5d58e9bb32b38d7211957f54b8fb410844e3544c
hiarica07 <halilarica@gmail.com>
2024-06-27 16:32:38 +0300

    Finish

--- app-2.js
+++ app-2.js
@@ -1,3 +1,66 @@
-// The task is to create a webpage that displays random user data, including 
-//! the user's title, first name, last name, email, phone, and thumbnail image, within a framed container.
+// Unchecked, other way old one 
 
+const button = document.querySelector("button")
+
+const getUser = async () =>{
+    try {
+        const response = await fetch("https://randomuser.me/api/")
+        const data = await response.json()
+        const randomUser = data.results[0]
+        console.log(randomUser);
+        return randomUser
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+    
+}
+
+const container = document.querySelector(".container")
+
+const renderUser = async () => {
+    const user = await getUser()
+
+    console.log(user);
+    
+    
+
+    const cardDiv = document.createElement("div")
+    cardDiv.classList.add("card")
+
+    const userImage = document.createElement("img")
+    userImage.classList.add("user-image")
+    userImage.src = user.picture.large
+    userImage.alt = "User Image"
+
+    const userList = document.createElement("ul")
+
+    const userName = document.createElement("li")
+    userList.textContent = `User: ${user.name.title} ${user.name.first} ${user.name.last}`
+
+    const userEmail = document.createElement("li")
+    userEmail.textContent = `${user.email}`
+
+    const userPhone = document.createElement("li")
+    userPhone.textContent = `${user.phone}`
+
+    const userThumbnail = document.createElement("img")
+    userThumbnail.src = user.picture.thumbnail
+    userThumbnail.alt = "User Image"
+
+
+
+
+    container.appendChild(cardDiv)
+    cardDiv.appendChild(userImage)
+    cardDiv.appendChild(userList)
+    userList.appendChild(userName)
+    userList.appendChild(userEmail)
+    userList.appendChild(userPhone)
+    cardDiv.appendChild(userThumbnail)
+}
+
+button.addEventListener("click", ()=>{
+    container.innerHTML = ""
+    renderUser()
+
+})
